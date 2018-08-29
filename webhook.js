3ba89c9fe0cff5c5c793afe3c41099a941c9e4b0
jalakpatoliya <30546867+jalakpatoliya@users.noreply.github.com>
2018-08-29 12:36:33 +0530

    testing apiai

--- webhook.js
+++ webhook.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const request = require('request');
+const apiaiApp = require('apiai')('c33a16ebf58c4397a657b18adc3fd532');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -39,17 +40,50 @@ app.post('/webhook', (req, res) => {
 
 function sendMessage(event) {
   let sender = event.sender.id;
-  let text = event.message.text;
-
-  request({
-    url: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: {access_token: 'EAAgOiVZAq7EkBAGqyMaxEeTVMoGFZAALwOikOWVKU3oUUOa9XNbgcyDZCnhSaNdbkXLBtKj7F2ePk1Lb8O8Y8UsoBILrJ9jaxZBLd0NoXyZCnHDkIXoXHDiZCneQfZBCjgWTM2Y8XQNZCiFQQzqeaZB72XrONOs7BshAXnfc6XCJJNAZDZD'},
-    method: 'POST',
-    json: {
-      recipient: {id: sender},
-      message: {text: text}
-    }
+  let text   = event.message.text;
+  let apiai  = apiaiApp.textRequest(text,{
+                sessionId:'tabby_cat'
   });
+
+  apiai.on('response',(response)=>{
+    request({
+      url: 'https://graph.facebook.com/v2.6/me/messages',
+      qs: {access_token: 'EAAgOiVZAq7EkBAGqyMaxEeTVMoGFZAALwOikOWVKU3oUUOa9XNbgcyDZCnhSaNdbkXLBtKj7F2ePk1Lb8O8Y8UsoBILrJ9jaxZBLd0NoXyZCnHDkIXoXHDiZCneQfZBCjgWTM2Y8XQNZCiFQQzqeaZB72XrONOs7BshAXnfc6XCJJNAZDZD'},
+      method: 'POST',
+      json: {
+        recipient: {id: sender},
+        message: {text: response}
+      }
+    }, function (error, response) {
+      if (error) {
+          console.log('Error sending message: ', error);
+      } else if (response.body.error) {
+          console.log('Error: ', response.body.error);
+      }
+    });
+  });
+
+  apiai.on('error',(error)=>{
+    console.log(error);
+  })
+
+  apiai.end();
+
+  // request({     // for echoing the text
+  //   url: 'https://graph.facebook.com/v2.6/me/messages',
+  //   qs: {access_token: 'EAAgOiVZAq7EkBAGqyMaxEeTVMoGFZAALwOikOWVKU3oUUOa9XNbgcyDZCnhSaNdbkXLBtKj7F2ePk1Lb8O8Y8UsoBILrJ9jaxZBLd0NoXyZCnHDkIXoXHDiZCneQfZBCjgWTM2Y8XQNZCiFQQzqeaZB72XrONOs7BshAXnfc6XCJJNAZDZD'},
+  //   method: 'POST',
+  //   json: {
+  //     recipient: {id: sender},
+  //     message: {text: text}
+  //   }
+  // }, function (error, response) {
+  //   if (error) {
+  //       console.log('Error sending message: ', error);
+  //   } else if (response.body.error) {
+  //       console.log('Error: ', response.body.error);
+  //   }
+  // });
 }
 
 const server = app.listen(process.env.PORT || 8080,()=> console.log("Server started at port %d in mode %s ", server.address().port,app.settings.env));
